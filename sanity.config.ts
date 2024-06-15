dde760e702e633f82367cf153b79a56502938973
Grilo <66530492+restyle3@users.noreply.github.com>
2024-06-16 00:31:36 +0100

    sanity configuration finished

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,9 @@
 'use client'
 
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
- */
-
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import schemas from './sanity/schemas'
 
@@ -17,12 +12,9 @@ const config = defineConfig({
   basePath: '/admin',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
   schema: { types: schemas },
   plugins: [
     structureTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
   ],
 })
---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,22 @@
+import { Project } from "@/types/Project";
+import { createClient, groq } from "next-sanity";
+
+export async function getProjects(): Promise<Project[]> {
+  const client = createClient({
+    projectId: "gq9vwtll",
+    dataset: "production",
+    apiVersion: "2024-06-15",
+  });
+
+  return client.fetch(
+    groq`*[_type == "project"]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`
+  );
+}
